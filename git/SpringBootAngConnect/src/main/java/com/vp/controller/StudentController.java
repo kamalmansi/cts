a509dfb740544723243857e422555f38fa89bbdd
kamalmansi <60208712+kamalmansi@users.noreply.github.com>
2020-02-20 17:43:13 +0530

    Add files via upload

---
+++ git/SpringBootAngConnect/src/main/java/com/vp/controller/StudentController.java
@@ -0,0 +1,47 @@
+package com.vp.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vp.model.Student;
+import com.vp.service.StudentService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+public class StudentController {
+
+	
+	@Autowired
+	StudentService studentService;
+	
+	
+	@GetMapping("/student")
+	public List<Student> listAllStudents(){ 
+		 
+   	 return (List<Student>) studentService.getAllStudent(); 
+    }
+	
+	
+	
+	@RequestMapping(value = "/student", method = RequestMethod.POST)
+	public Student saveStudent(@RequestBody Student student) {
+		return studentService.addStudent(student);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
