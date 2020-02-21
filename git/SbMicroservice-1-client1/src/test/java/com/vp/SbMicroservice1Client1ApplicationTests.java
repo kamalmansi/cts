f895605c9e7a240d4d71c118717371c87b8a1073
kamalmansi <60208712+kamalmansi@users.noreply.github.com>
2020-02-21 12:34:59 +0530

    Add files via upload

---
+++ git/SbMicroservice-1-client1/src/test/java/com/vp/SbMicroservice1Client1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMicroservice1Client1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ git/SbMicroservice/src/main/java/com/vp/SbMicroserviceApplication.java
@@ -0,0 +1,27 @@
+package com.vp;
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+
+@SpringBootApplication
+
+@EnableEurekaServer
+public 
+
+class SbMicroserviceApplication {
+
+	
+public static void main(String[] args) {
+		
+SpringApplication.run(SbMicroserviceApplication.class, args);
+	
+}
+
+
+}
